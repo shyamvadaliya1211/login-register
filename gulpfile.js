45245d59c4edb7b7204ef0cf732a25ecfade1f52
shyam <shyam.lightwebs@gmail.com>
2019-09-17 14:37:53 +0530

    Basic Set Up View

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+const ngAnnotate = require('gulp-ng-annotate');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const sequence = require('gulp-sequence');
+
+gulp.task('default', () => {
+    nodemon({
+        script: 'index.js',
+        ext: 'js html',
+        env: {},
+    });
+});
+
+const allCSSFiles = [
+    './node_modules/bootstrap/dist/css/bootstrap.min.css',
+];
+
+gulp.task('min-css', () => gulp.src(allCSSFiles)
+    .pipe(cleanCSS({
+        compatibility: 'ie8',
+        rebase: false,
+    }))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./public/build/')));
+
+
+const allJsFiles = [
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './node_modules/angular/angular.min.js',
+];
+
+gulp.task('min-js', () => gulp.src(allJsFiles)
+    .pipe(concat('script.min.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify({
+        mangle: false,
+    }).on('error', console.error))
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest('public/build/')));
